482fb20f564041e8c53ffe99eb72677f1d6607c5
Hannu Jaakkola <hannujaak@gmail.com>
2016-11-26 18:45:31 +0200

    Improvements

--- script.js
+++ script.js
@@ -47,23 +47,26 @@ function initMap() {
   });
 
   directionsService = new google.maps.DirectionsService;
-  directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true });
-  busRouteDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true, polylineOptions: busPolylineOptions });
+  busRouteDisplay = new google.maps.DirectionsRenderer({preserveViewport: true, suppressMarkers: true, polylineOptions: busPolylineOptions });
   var geocoder = new google.maps.Geocoder()
 
-  console.log(destination);
+  preserveViewport = true
   if (destination) {
     geocode(geocoder, destination)
+    preserveViewport = false
   }
+  directionsDisplay = new google.maps.DirectionsRenderer({preserveViewport: preserveViewport, suppressMarkers: true });
 
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     disableDefaultUI: true,
-    center: {lat: 60.1656706, lng: 24.9650129},
+    center: {lat: 60.165873, lng: 24.966461},
     styles: [{"elementType":"geometry","stylers":[{"color":"#f5f5f5"}]},{"elementType":"labels","stylers":[{"visibility":"off"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f5f5"}]},{"featureType":"administrative.land_parcel","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"administrative.neighborhood","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#dadada"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#8DB0FF"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]}]
   });
   busRouteDisplay.setMap(map);
   directionsDisplay.setMap(map);
+
+  addMarker(new google.maps.LatLng(60.165873, 24.966461), 'images/MapView_LocationPointer_supersmall.png')
 
   google.maps.event.addListener(map, "click", function (e) {
     destinationLocation = e.latLng
@@ -123,6 +126,7 @@ function calculateAndDisplayBusRoute() {
 
 function calculateAndDisplayRoute(stop) {
   var origin = new google.maps.LatLng(60.165873, 24.966461)
+
   var startStop = new google.maps.LatLng(60.165622, 24.966118)
   directionsService.route({
     origin: origin,
@@ -179,7 +183,7 @@ function addBus(route, busLocation) {
 }
 
 function start() {
-  mapTitle.innerHTML = 'Leave in 1 minute'
+  mapTitle.innerHTML = 'Next in 2 minutes'
   document.getElementById('ok').style.display = 'none'
   window.setTimeout(function() {
     App.load('page4');
@@ -188,7 +192,7 @@ function start() {
       App.load('page5')
       window.setTimeout(function() {
         App.load('home')
-      }, 10 * 1000)
+      }, 5 * 1000)
     }, 5 * 1000)
   }, 5 * 1000)
 }
