4300455a4652b4b7845266906aebf795aab64689
Hannu Jaakkola <hannujaak@gmail.com>
2016-11-26 14:57:34 +0200

    Fix

--- script.js
+++ script.js
@@ -33,18 +33,16 @@ function mapReady() {
 function initMap() {
   var directionPolylineOptions = new google.maps.Polyline({
     strokeColor: '#3DD3A1',
-    strokeOpacity: .5,
-    strokeWeight: 10
   });
 
   var busPolylineOptions = new google.maps.Polyline({
     strokeColor: '#000',
-    strokeOpacity: .5,
+    strokeOpacity: .25,
     strokeWeight: 5
   });
 
   directionsService = new google.maps.DirectionsService;
-  directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true, polylineOptions: directionPolylineOptions });
+  directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true });
   busRouteDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true, polylineOptions: busPolylineOptions });
   var geocoder = new google.maps.Geocoder()
 
@@ -55,8 +53,8 @@ function initMap() {
     center: {lat: 60.1656706, lng: 24.9650129},
     styles: [{"elementType":"geometry","stylers":[{"color":"#f5f5f5"}]},{"elementType":"labels","stylers":[{"visibility":"off"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f5f5"}]},{"featureType":"administrative.land_parcel","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"administrative.neighborhood","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#dadada"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#8DB0FF"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]}]
   });
-  directionsDisplay.setMap(map);
   busRouteDisplay.setMap(map);
+  directionsDisplay.setMap(map);
 
   mapTitle = document.getElementById('map-title')
 
@@ -64,7 +62,7 @@ function initMap() {
     destinationLocation = e.latLng
 
     var stop = calculateStop(destinationLocation)
-    // calculateAndDisplayBusRoute();
+    calculateAndDisplayBusRoute();
     calculateAndDisplayRoute(stop);
 });
 
