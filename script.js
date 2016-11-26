a0e4577df10308728f1b94004b2c8015bd0a0c4f
Hannu Jaakkola <hannujaak@gmail.com>
2016-11-26 14:48:34 +0200

    Custom markers

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var directionsService
 var directionsDisplay
+var busRouteDisplay
 var stopLocations = []
 var destination
 var destinationLocation
+var map
+var mapTitle
 
 function addDestination() {
   destination = document.getElementById('destination').value + ', helsinki'
@@ -28,23 +31,40 @@ function mapReady() {
 }
 
 function initMap() {
+  var directionPolylineOptions = new google.maps.Polyline({
+    strokeColor: '#3DD3A1',
+    strokeOpacity: .5,
+    strokeWeight: 10
+  });
+
+  var busPolylineOptions = new google.maps.Polyline({
+    strokeColor: '#000',
+    strokeOpacity: .5,
+    strokeWeight: 5
+  });
+
   directionsService = new google.maps.DirectionsService;
-  directionsDisplay = new google.maps.DirectionsRenderer;
+  directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true, polylineOptions: directionPolylineOptions });
+  busRouteDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true, polylineOptions: busPolylineOptions });
   var geocoder = new google.maps.Geocoder()
 
   geocode(geocoder, destination)
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: {lat: 60.1656706, lng: 24.9650129},
     styles: [{"elementType":"geometry","stylers":[{"color":"#f5f5f5"}]},{"elementType":"labels","stylers":[{"visibility":"off"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f5f5"}]},{"featureType":"administrative.land_parcel","stylers":[{"visibility":"off"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"administrative.neighborhood","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#dadada"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#8DB0FF"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]}]
   });
   directionsDisplay.setMap(map);
+  busRouteDisplay.setMap(map);
+
+  mapTitle = document.getElementById('map-title')
 
   google.maps.event.addListener(map, "click", function (e) {
     destinationLocation = e.latLng
 
     var stop = calculateStop(destinationLocation)
+    // calculateAndDisplayBusRoute();
     calculateAndDisplayRoute(stop);
 });
 
@@ -76,19 +96,55 @@ function geocode(geocoder, address) {
   })
 }
 
-function calculateAndDisplayRoute(stop) {
+function calculateAndDisplayBusRoute() {
   directionsService.route({
-    origin: new google.maps.LatLng(60.165873, 24.966461),
-    waypoints: [{ location: new google.maps.LatLng(60.165622, 24.966118) }, { location: stop }],
-    destination: destinationLocation,
-    travelMode: 'WALKING'
+    origin: new google.maps.LatLng(60.165918, 24.975119),
+    destination: stopLocations[stopLocations.length-1],
+    travelMode: 'DRIVING',
   }, function(response, status) {
     if (status === 'OK') {
-      directionsDisplay.setDirections(response);
+      busRouteDisplay.setDirections(response);
     } else {
       window.alert('Directions request failed due to ' + status);
     }
   });
+}
+
+function calculateAndDisplayRoute(stop) {
+  var origin = new google.maps.LatLng(60.165873, 24.966461)
+  var startStop = new google.maps.LatLng(60.165622, 24.966118)
+  directionsService.route({
+    origin: origin,
+    waypoints: [{ location: startStop }, { location: stop }],
+    destination: destinationLocation,
+    travelMode: 'WALKING',
+  }, function(response, status) {
+    if (status === 'OK') {
+      mapTitle.innerHTML = 'Walk to the first stop'
+      directionsDisplay.setDirections(response);
+
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+      }
+      markers = []
+      addMarker(origin, 'images/MapView_LocationPointer_supersmall.png')
+      addMarker(destinationLocation, {
+        url: 'images/RouteFlag_supersmall.png',
+        anchor: new google.maps.Point(-1, 28),
+      })
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
+
+var markers = []
+function addMarker(position, icon) {
+  markers.push(new google.maps.Marker({
+    position: position,
+    map: map,
+    icon: icon,
+  }));
 }
 
 App.controller('home', function (page) {
